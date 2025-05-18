e9069cfcb3fdd825eaa53d41a889d866cc6eb334
quốc bảo Trần <quocbaotran@trbao335-2.local>
2025-05-18 20:25:06 +0700

    Updates

--- redirect.js
+++ redirect.js
@@ -2,9 +2,13 @@
 const user = JSON.parse(localStorage.getItem("user"));
 
 if (!user) {
-    window.location.href = "../../LogInPage/index.html";
+    window.location.href = "../../LogInPage/index.html";s
+}
+
+const session = JSON.parse(localStorage.getItem("session"));
+if (!session || Date.now() - session.loginTime > session.expiresIn) {
+  localStorage.removeItem("session");
+  window.location.href = "index.html";
 }
 
 // http://192.168.1.9:5504/LogInPage/index.html
-// http://192.168.1.9:5503/Leader/ProjectPage/index.html
-// http://192.168.1.9:5503/Leader/UserPage/index.html
